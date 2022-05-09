88c79e14f9f7818311a1fa499a42f4f09a52e2c3
CedricBaude <baude.cedric.perso@gmail.com>
2022-05-09 22:06:16 +0200

    construction fonction

--- functions/cedric.php
+++ functions/cedric.php
@@ -5,6 +5,28 @@
 function checkPassword($password) {
 
     echo '<h1 class="text-center">Cedric</h1>';
+    echo '<div class"row">
+            <p>Mot de passe: ' .$password;
+    echo ' </p>
+            <p class=text-end >Force du mot de passe </p>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <br>
+
+
+            <div class=" row col-3 align-self-center">
+                <ul class="list-group ">
+                <li class="list-group-item active" aria-current="true">An active item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+                </ul>
+            </div>
+
+
+        </div>';
 
     // ...
 
